5bd20ee65bf748623eb613221bec6a267f9282e3
sjfox <sjfcodes@gmail.com>
2024-04-03 20:23:54 -0700

    tinkering

--- client/script.js
+++ client/script.js
@@ -1,19 +1,32 @@
-const stream1Ul = document.getElementById('stream1');
-const stream2Ul = document.getElementById('stream2');
-const eventSrc = new EventSource('http://localhost:3000/events');
+const stream1Ul = document.getElementById("date");
+const stream2Ul = document.getElementById("loading");
 
+const onDateEvent = (event) => {
+  const ulEl = document.createElement("li");
+  ulEl.textContent = event.data;
+  stream1Ul.prepend(ulEl);
+};
 
-const handleStream1 = (event) => {
-    const ulEl = document.createElement('li');
-    ulEl.textContent = event.data;
-    stream1Ul.appendChild(ulEl);
+const onColorsEvent = (event) => {
+  const {r,g,b} = JSON.parse(event.data)
+  const color = `rgb(${r}, ${g}, ${b})`
+  stream2Ul.textContent = color;
+  stream2Ul.style.color = color;
+};
+
+function addEventSrc(
+  url = "",
+  path = "",
+  handlers = [["default", () => null]]
+) {
+  const sse = new EventSource(url + path);
+  if (typeof handlers === 'function') {
+    sse.onmessage = handlers;
+  } else {
+    handlers.forEach(([event, cb]) => sse.addEventListener(event, cb));
   }
+  return sse;
+}
 
-const handleStream2 = (event) => {
-    const ulEl = document.createElement('li');
-    ulEl.textContent = event.data;
-    stream2Ul.appendChild(ulEl);
-  }
-
-  eventSrc.addEventListener('stream1', handleStream1);
-eventSrc.addEventListener('stream2', handleStream2);
+addEventSrc("http://localhost:3000", "/date?interval=1000", [["stream0", onDateEvent]])
+addEventSrc("http://localhost:3000", "/colors", onColorsEvent)
--- server/index.js
+++ server/index.js
@@ -1,26 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.get('/events', (req, res) => {
-  const headers = {
-    'Content-Type': 'text/event-stream',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache'
-  };
-  res.writeHead(200, headers);
-  setInterval(()=>res.write(`event: stream1\ndata: ${JSON.stringify({date: new Date().toLocaleTimeString()})}\n\n`),1000)
-  setInterval(()=>res.write(`event: stream2\ndata: ${JSON.stringify({date: new Date().toLocaleTimeString()})}\n\n`),2000)
-  req.on('close', () => console.log(`Connection closed`));
+app.use(bodyParser.urlencoded({ extended: false }));
+const sseHeaders = {
+  "Content-Type": "text/event-stream",
+  Connection: "keep-alive",
+  "Cache-Control": "no-cache",
+};
+
+app.get("/date", (req, res) => {
+  res.writeHead(200, sseHeaders);
+
+  setInterval(() => {
+    res.write(
+      `event: stream0\ndata: ${JSON.stringify({
+        date: new Date().toISOString(),
+      })}\n\n`
+    );
+  }, Number(req.query.interval));
+  req.on("close", () => console.log(`Connection closed`));
+});
+
+app.get("/colors", (req, res) => {
+  res.writeHead(200, sseHeaders);
+
+  const getColor = () => Math.floor(Math.random() * 256);
+
+  setInterval(() => {
+    let colors = { r: getColor(), g: getColor(), b: getColor() };
+    res.write(`data: ${JSON.stringify(colors)}\n\n`);
+  }, 3000);
+  req.on("close", () => console.log(`Connection closed`));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
-  
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
